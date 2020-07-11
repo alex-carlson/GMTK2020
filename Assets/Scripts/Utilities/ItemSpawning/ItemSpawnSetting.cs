5e951ec33dd4f16d79fc42c08c866c4a2c085993
Zack <azackd@gmail.com>
2020-07-11 16:11:10 -0300

    Party Item Spawner made

---
+++ Assets/Scripts/Utilities/ItemSpawning/ItemSpawnSetting.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawnSetting : MonoBehaviour
+{
+    public SurfaceType[] legalSpawnSurfaces;
+    public GameObject itemPrefab;
+}
